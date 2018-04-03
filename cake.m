f6d84d0e6103f4f3b499c2c09e583e901fa9dc7d
Leman FENG <flm8620@gmail.com>
2018-04-03 22:38:22 +0200

    delete cake.m

--- cake.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = cake(inputArg1,inputArg2)
-%CAKE Summary of this function goes here
-%   cake function (equation (7))
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
